9310d25547a0f38480278a25dc940c118120b4a3
dianakakoma <dikakoma@gmail.com>
2021-03-07 09:20:59 -0600

    connected routes to their HTML pages.

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-    return"<p>Logout</p>"
+    return "<h3>Logoout</h3>"
 
 @auth.route('/sign-up')
 def signUp():
-    return "<p>Sign_up </p>"
+    return render_template("sign_up.html")
