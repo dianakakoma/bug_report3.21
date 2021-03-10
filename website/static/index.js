da1f36b1e292208edea8b787f8a6cf48618b05fe
dianakakoma <dikakoma@gmail.com>
2021-03-09 22:54:39 -0600

    we can add new rports, but the delete button is not working.

---
+++ website/static/index.js
@@ -0,0 +1,8 @@
+function deleteReport(reportId){
+    fetch('/delete-report', {
+        method: 'POST',
+        body: JSON.stringify({ reportId: reportId }),
+    }).then((_res)) => {
+        window.location.href = '/';
+    });
+}
--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
 from . import db
 from .models import Report
+import json
 
 views = Blueprint('views', __name__)
 
@@ -19,3 +20,15 @@ def home():
             db.session.commit()
             flash('Report added!', category='success')
     return render_template("home.html", user=current_user)
+
+@views.route('/delete-report',methods=['POST'])
+def delete_report():
+    report = json.loads(request.data)
+    reportId = report['reportId']
+    report = Report.query.get(reportId)
+    if report:
+        #user can only delete their own notes
+        if report.user_id == current_user.id:
+            db.session.delete(report)
+            db.session.commit()
+    return jsonify({})
